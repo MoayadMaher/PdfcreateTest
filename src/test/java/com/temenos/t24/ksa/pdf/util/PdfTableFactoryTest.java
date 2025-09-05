a3bbc10d42999a6641179a1a29246dcbd21e86b5
Moayad Maher <148288780+MoayadMaher@users.noreply.github.com>
2025-09-05 16:57:05 +0300

    Add unit tests and JUnit setup

---
+++ src/test/java/com/temenos/t24/ksa/pdf/util/PdfTableFactoryTest.java
@@ -0,0 +1,38 @@
+package com.temenos.t24.ksa.pdf.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.MalformedURLException;
+
+import org.junit.jupiter.api.Test;
+
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.layout.element.Table;
+import com.temenos.t24.ksa.pdf.model.IbanDetails;
+import com.temenos.t24.ksa.pdf.model.InvoiceData;
+
+public class PdfTableFactoryTest {
+
+    @Test
+    public void createInfoHeaderTableStructure() throws Exception {
+        InvoiceData data = new InvoiceData();
+        data.iban = new IbanDetails();
+        data.iban.ibanNumber = "IBAN123";
+        data.iban.taxRegistrationNumber = "TRN";
+
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        Table table = PdfTableFactory.createInfoHeaderTable(data, font);
+        assertEquals(2, table.getNumberOfColumns());
+        assertEquals(4, table.getChildren().size());
+    }
+
+    @Test
+    public void createHeaderTableInvalidLogoPathThrows() throws Exception {
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        assertThrows(MalformedURLException.class,
+                () -> PdfTableFactory.createHeaderTable(font, "bad://path"));
+    }
+}
+
